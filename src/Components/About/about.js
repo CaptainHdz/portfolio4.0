7926e8912f117acf3184b167a0b48b5256813cf6
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2019-12-15 00:10:35 -0600

    additional styling

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -18,7 +18,7 @@ const About = () => {
     <hr className="divider" />
     <div className="container">
     <div id="about-row-1" className="row">
-        <div id="left" data-aos="fade-right" data-aos-duration="1000" className="col s12 m6 white-text">
+        <div id="left" data-aos="fade-right" data-aos-duration="500" className="col s12 m6 white-text">
           <h3 id="introduction"  >
               <span id="">Hi!</span><br/>
               <span id="sentence2">I'm Mauricio Hernandez.</span><br/>
@@ -31,7 +31,7 @@ const About = () => {
             </div>
           </div>
         </div>
-        <p data-aos="fade-left" data-aos-duration="1000" id="summary" className="col s12 m6 white-text right-align">
+        <p data-aos="fade-left" data-aos-duration="2000" id="summary" className="col s12 m6 white-text right-align">
        <span id="summ-intro"> I am a dedicated software developer</span> that takes pride in creating excellent products. I have a real fascination for bringing ideas to life. Nothing is more rewarding than seeing the final outcome of every brick being layed. I am tailored in some of the latest technology to deliver cutting edge results to your screen. All the way from Houston, Texas, this developer is ready to serve and bring a smile to everyone, from small projects, to big business. Let's build something together.  
         </p>
       </div>
