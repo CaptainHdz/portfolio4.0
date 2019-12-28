e8c415f48131bc18b145df60b7706c08adcec930
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2019-12-27 23:06:08 -0600

    Added form concept w/ formspree, footer concept

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -4,10 +4,25 @@ import './style.css'
 
 const Contact = () => {
     return (
-        <div id="contact">
+      <div id="contact" className="black">
+        <h2 id="contact-header">Ready To Work Together?</h2>
+        <hr className="contact-divider" />
+        <div className="container">        
+          <form action="https://formspree.io/mayqdarw" method="POST">
+            {/* <label>Your email:</label> */}
+            <input className="form-width" type="text" placeholder="Email" name="_replyto" />
 
+            {/* <label>Your Name:</label> */}
+            <input className="form-width" placeholder="Your Name" type="text" name="name" />
+
+            {/* <label>Your message:</label> */}
+            <textarea id="form-textbox" placeholder="Your Message" name="message"></textarea>
+
+            <button type="submit">Send</button>
+          </form>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Contact;
