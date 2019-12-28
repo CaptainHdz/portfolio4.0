e29d9ef2b96197ef2ebfe6f56031d7fc506cf0f3
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2019-12-28 14:37:15 -0600

    cleaned up contact form

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -8,17 +8,17 @@ const Contact = () => {
         <h2 id="contact-header" data-aos="fade-left" data-aos-duration="1000">Ready To Work Together?</h2>
         <hr className="contact-divider" />
         <div className="container">        
-          <form action="https://formspree.io/mayqdarw" method="POST">
+          <form action="https://formspree.io/mayqdarw" method="POST" className="row">
             {/* <label>Your email:</label> */}
-            <input className="form-width" type="text" placeholder="Email" name="_replyto" />
+            <input className="col s8 offset-s2" type="text" placeholder="Email" name="_replyto" />
 
             {/* <label>Your Name:</label> */}
-            <input className="form-width" placeholder="Your Name" type="text" name="name" />
+            <input className="col s8 offset-s2" placeholder="Your Name" type="text" name="name" />
 
             {/* <label>Your message:</label> */}
-            <textarea id="form-textbox" placeholder="Your Message" name="message"></textarea>
+            <textarea id="form-textbox" className="col s12 offset-s3" placeholder="Your Message" name="message"></textarea>
 
-            <button type="submit">Send</button>
+            <button id="form-button" type="submit" className="col s4 offset-s4 btn waves-effect waves-light">Send</button>
           </form>
         </div>
       </div>
