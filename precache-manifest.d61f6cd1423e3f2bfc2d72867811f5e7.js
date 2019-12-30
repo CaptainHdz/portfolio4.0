09a32e44580def0271a7764bca927783864cb06f
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2019-12-30 12:56:40 -0600

    Updates

---
+++ precache-manifest.d61f6cd1423e3f2bfc2d72867811f5e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8615030b6ac86e8a5c6e40fc17b23c9",
+    "url": "/portfolio4.0/index.html"
+  },
+  {
+    "revision": "f382dab65a500b04081d",
+    "url": "/portfolio4.0/static/css/main.7b53b9b4.chunk.css"
+  },
+  {
+    "revision": "df5ff3ece84f3b708028",
+    "url": "/portfolio4.0/static/js/2.292b6235.chunk.js"
+  },
+  {
+    "revision": "77ab068d8694e758f390d677019529d6",
+    "url": "/portfolio4.0/static/js/2.292b6235.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f382dab65a500b04081d",
+    "url": "/portfolio4.0/static/js/main.68fa8bd1.chunk.js"
+  },
+  {
+    "revision": "92860569c7e0ca3b7e01",
+    "url": "/portfolio4.0/static/js/runtime-main.9d6e0c12.js"
+  },
+  {
+    "revision": "a490335751dbbd80c575d237e7d70e2d",
+    "url": "/portfolio4.0/static/media/Dollar-Scrape.a4903357.PNG"
+  },
+  {
+    "revision": "633d0b133cec793e3a66aff4d127c579",
+    "url": "/portfolio4.0/static/media/GIPHY.633d0b13.png"
+  },
+  {
+    "revision": "276683ae6640c0502b047bd0fdacf44a",
+    "url": "/portfolio4.0/static/media/IMG_5620.276683ae.jpg"
+  },
+  {
+    "revision": "e5161e4d98330d630fd72aa7b97b5582",
+    "url": "/portfolio4.0/static/media/TradED.e5161e4d.png"
+  },
+  {
+    "revision": "558d96ae8c66b7140162fb9f4652fe2f",
+    "url": "/portfolio4.0/static/media/blue-space.558d96ae.jpg"
+  },
+  {
+    "revision": "7044dfb5e5ddbd4d190707284d85b4c2",
+    "url": "/portfolio4.0/static/media/clicky-game-zoom.7044dfb5.png"
+  },
+  {
+    "revision": "690dc4b93598d237cb282f0f309eb9a7",
+    "url": "/portfolio4.0/static/media/coming-soon.690dc4b9.png"
+  }
+]);
