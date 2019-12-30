2458187e4863c2f4b2d7f74a859e8ccdcfcd3a66
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2019-12-30 02:43:09 -0600

    working icons

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import './style.css';
-import '@fortawesome/fontawesome-free';
-import Github from './Icons/Github-icon.png';
-import LinkedIn from './Icons/LinkedIN-icon.png';
-
+import React from "react";
+import "./style.css";
+import "@fortawesome/fontawesome-free";
 
 const Footer = () => {
-    return (
-      <div id="footer">
-        <footer className="page-footer black">
-          {/* <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">Footer Content</h5>
-                
-              </div>
-              <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Links</h5>
-                
-              </div>
-            </div>
-          </div> */}
-          <div className="footer-copyright black">
-            <div className="container">
-             <div className="left">© 2019 Mauricio Hernandez</div> 
+  return (
+    <div id="footer">
+      <footer className="page-footer black">
+        <div className="footer-copyright black">
+          <div className="container">
+            <div className="left">© 2019 Mauricio Hernandez</div>
+            <a className="grey-text" href="https://github.com/CaptainHdz">
               <i className="fa fa-github-alt github small right"></i>
+            </a>
+            <a className="grey-text" href="https://www.linkedin.com/in/mauricio-hernandez-54170617b/">
               <i class="fa fa-linkedin linked-in small right"></i>
-             </div>
+            </a>
           </div>
-        </footer>
-      </div>
-    );
-}
-
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
