35617563680311ba918673996eb671aa100688f6
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2019-12-30 01:08:44 -0600

    Working card links

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -11,34 +11,43 @@ import ComingSoon from './Images/coming-soon.png';
 const Portfolio = () => {
     return (
         <div id="portfolio" className="grey darken-4">
-            <h2 id="portfolio-header" className="row" data-aos="fade-down" data-aos-delay="500">Check Out My Work</h2>
+            <h2 id="portfolio-header" className="row" data-aos="fade-down" data-aos-duration="100" data-aos-delay="500">Check Out My Work</h2>
             <hr className="divider" />
             <div id="portfolio-container" className="container" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="400">
                 <div className="row">
                 <Card
                 image={clickyGame}
                 text="Simple and Fun Memory Game"
+                link="https://captainhdz.github.io/clicky-pokemonsters/"
+                buttonClass="btn-large waves-effect"
                 />
                 <Card
                 image={Giphy}
                 text="Web App that Searches for GIFs"
+                link="https://captainhdz.github.io/giphy-app/"
+                buttonClass="btn-large waves-effect"
                 />
                 <Card
                 image={Scraper}
                 text="Save, and Read Financial News"
+                link="https://tranquil-denali-preserve-30101.herokuapp.com/"
+                buttonClass="btn-large waves-effect"
                 />
                 <Card
                 image={Traded}
                 text="Educative Stock Market Platform"
+                link="https://traded1.herokuapp.com/"
+                buttonClass="btn-large waves-effect"
                 />
                 <Card
                 image={ComingSoon}
                 text="More Apps Coming Soon"
+                buttonClass="btn-large disabled btn-disabled"
                 />
                 <Card
                 image={ComingSoon}
                 text="More Apps Coming Soon"
-
+                buttonClass="btn-large disabled btn-disabled"
                 />
                 </div>
             </div>
--- src/Components/ProjectCard/card.js
+++ src/Components/ProjectCard/card.js
@@ -4,12 +4,12 @@ import './style.css';
  
 const Card = (props) => {
 return (
-    <div className="project-card col m4">
+    <div className="project-card col s12 m4">
         <div class="card-image waves-effect waves-block waves-light">
             <img class="activator card-image" src={props.image} alt="project img" />
             <div className="card-overlay white-text">
                 <h3 className="card-header">{props.text}</h3>
-                <button id="card-button" className="btn">Visit</button>
+                <a href={props.link} target="_blank" rel="noopener noreferrer" id="card-button" className={props.buttonClass}>Visit</a>
             </div>
         </div>
     </div>
