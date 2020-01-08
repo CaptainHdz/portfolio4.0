3a840d2603f5f0d7f6db82056218ef15478b7ced
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-07 23:55:51 -0600

    Added new project to portfolio

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
 import Card from '../ProjectCard/card';
-import clickyGame from './Images/clicky-game-zoom.png';
+import ClickyGame from './Images/clicky-game-zoom.png';
 import Giphy from './Images/GIPHY.png';
 import Scraper from './Images/Dollar-Scrape.PNG';
 import Traded from './Images/TradED.png';
 import ComingSoon from './Images/coming-soon.png';
+import BookTastic from './Images/Book-Finder2-screenshot.jpg';
 
 
 const Portfolio = () => {
@@ -13,20 +14,15 @@ const Portfolio = () => {
         <div id="portfolio" className="grey darken-4">
             <h2 id="portfolio-header" className="row" data-aos="fade-down" data-aos-duration="100" data-aos-delay="500">Check Out My Work</h2>
             <hr className="divider" />
-            <div id="portfolio-container" className="container row" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="400">
+            <div id="portfolio-container" className="container row" data-aos="fade-right" data-aos-duration="1200" data-aos-delay="400">
                 <div className="">
                 <Card
-                image={clickyGame}
+                image={ClickyGame}
                 text="Simple and Fun Memory Game"
                 link="https://captainhdz.github.io/clicky-pokemonsters/"
                 buttonClass="btn-large waves-effect"
                 />
-                <Card
-                image={Giphy}
-                text="Web App that Searches for GIFs"
-                link="https://captainhdz.github.io/giphy-app/"
-                buttonClass="btn-large waves-effect"
-                />
+                
                 <Card
                 image={Scraper}
                 text="Save, and Read Financial News"
@@ -40,9 +36,16 @@ const Portfolio = () => {
                 buttonClass="btn-large waves-effect"
                 />
                 <Card
-                image={ComingSoon}
-                text="More Apps Coming Soon"
-                buttonClass="btn-large disabled btn-disabled"
+                image={Giphy}
+                text="Web App that Searches for GIFs"
+                link="https://captainhdz.github.io/giphy-app/"
+                buttonClass="btn-large waves-effect"
+                />
+                <Card
+                image={BookTastic}
+                text="Search A Huge Variety Of Books"
+                link="https://book-finder2.herokuapp.com/"
+                buttonClass="btn-large waves-effect"
                 />
                 <Card
                 image={ComingSoon}
