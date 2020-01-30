6a928a8178caa95ab1426c08fea5bcc3eab013e9
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-30 06:05:21 -0600

    Updates

---
+++ precache-manifest.161c14b5441be62e8930ade55a45c0bc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8273601a8012cd126aa8e4fec137fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f44211a46f5cef0e36d",
+    "url": "/static/css/main.e40104c1.chunk.css"
+  },
+  {
+    "revision": "6f33cf6db25eec49ebe5",
+    "url": "/static/js/2.174585b6.chunk.js"
+  },
+  {
+    "revision": "d7a8859e4271d0114de7250d18379d4d",
+    "url": "/static/js/2.174585b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f44211a46f5cef0e36d",
+    "url": "/static/js/main.9a0deb22.chunk.js"
+  },
+  {
+    "revision": "4d46a89461a414c89f66",
+    "url": "/static/js/runtime-main.25cc8cd7.js"
+  },
+  {
+    "revision": "f26210e9161a58b37fdd886784a9329d",
+    "url": "/static/media/Book-Finder2-screenshot.f26210e9.jpg"
+  },
+  {
+    "revision": "9652b19011670cabf773a9efb35f98fe",
+    "url": "/static/media/BookTastic.9652b190.mp4"
+  },
+  {
+    "revision": "a490335751dbbd80c575d237e7d70e2d",
+    "url": "/static/media/Dollar-Scrape.a4903357.PNG"
+  },
+  {
+    "revision": "6a4fcede9548514170ac3cfa69273067",
+    "url": "/static/media/GIF-Demo.6a4fcede.mp4"
+  },
+  {
+    "revision": "633d0b133cec793e3a66aff4d127c579",
+    "url": "/static/media/GIPHY.633d0b13.png"
+  },
+  {
+    "revision": "276683ae6640c0502b047bd0fdacf44a",
+    "url": "/static/media/IMG_5620.276683ae.jpg"
+  },
+  {
+    "revision": "ed3c09446505406d0ab5fbc5bafb9024",
+    "url": "/static/media/Pokememory.ed3c0944.mp4"
+  },
+  {
+    "revision": "3281db0df2fda069829674d31df5cc26",
+    "url": "/static/media/Power-List.3281db0d.mp4"
+  },
+  {
+    "revision": "a3a65272c6ac16824bb6827be90e8bcc",
+    "url": "/static/media/TradED.a3a65272.mp4"
+  },
+  {
+    "revision": "e5161e4d98330d630fd72aa7b97b5582",
+    "url": "/static/media/TradED.e5161e4d.png"
+  },
+  {
+    "revision": "83c1b4570c7d5026d90149bd0fa2aa52",
+    "url": "/static/media/World-Finance-Report.83c1b457.mp4"
+  },
+  {
+    "revision": "558d96ae8c66b7140162fb9f4652fe2f",
+    "url": "/static/media/blue-space.558d96ae.jpg"
+  },
+  {
+    "revision": "7044dfb5e5ddbd4d190707284d85b4c2",
+    "url": "/static/media/clicky-game-zoom.7044dfb5.png"
+  },
+  {
+    "revision": "76b6f98f8aa4d77f3944e291a68eed42",
+    "url": "/static/media/power-list.76b6f98f.PNG"
+  }
+]);
