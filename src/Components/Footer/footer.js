ebaa2b4c2a7d9c76a8d4e6a9111731ae0f9b1af5
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-02-14 01:39:58 -0600

    changed github link in footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="footer-copyright black">
           <div className="container">
             <div className="left">© 2020 Mauricio Hernandez</div>
-            <a className="grey-text" href="https://github.com/CaptainHdz" target="_blank" rel="noopener noreferrer">
+            <a className="grey-text" href="https://github.com/Mauro-hdz" target="_blank" rel="noopener noreferrer">
               <i className="fa fa-github-alt github small right"></i>
             </a>
             <a className="grey-text" href="https://www.linkedin.com/in/mauricio-hernandez-54170617b/" target="_blank" rel="noopener noreferrer">
