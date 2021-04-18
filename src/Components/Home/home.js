f567348a2ea2f6d5d0945c5cf70797f93c25183a
Mauro-hdz <47486440+CaptainHdz@users.noreply.github.com>
2021-04-17 19:39:00 -0500

    removed state from home component

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -4,13 +4,6 @@ import Particles from 'react-particles-js';
 import JSON from '../../particlesjs-config (1).json';
 
 class Home extends Component {
-	state = {
-		sentence1: 'Building The Future.',
-		sentence2: ' One Line Of Code At A Time.',
-	};
-
-	componentDidMount() {}
-
 	onClickDownArrow = () => {
 		const about = document.getElementById('about');
 		about.scrollIntoView({ behavior: 'smooth' });
@@ -21,8 +14,8 @@ class Home extends Component {
 			<div id='home' data-aos='zoom-in' data-aos-duration='1200'>
 				<Particles className='canvas-wrapper' canvasClassName='particles' params={JSON} />
 				<h1 id='headline'>
-					<span id='sentence1'>{this.state.sentence1}</span>
-					<span id='sentence2'>{this.state.sentence2}</span>
+					<span id='sentence1'>Building The Future.</span>
+					<span id='sentence2'>{' One Line Of Code At A Time.'}</span>
 				</h1>
 				<i
 					id='down-arrow'
